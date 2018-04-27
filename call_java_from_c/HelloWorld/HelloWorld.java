d0fa1885f40eafc9e92c71f4b8484e9de1327a44
Zimu Yang <zimu_yang@csil-03.cs.ucsb.edu>
2018-04-27 14:00:24 -0700

    JNI interface

---
+++ call_java_from_c/HelloWorld/HelloWorld.java
@@ -0,0 +1,13 @@
+public class HelloWorld{
+	public static void main(String[] args){
+		System.out.println("hi!");
+	}
+	public static int square(int input){
+		int output=input+5;
+		return output;
+	}
+
+	public static int power(int input, int times){
+		return input*times;
+	}
+}
