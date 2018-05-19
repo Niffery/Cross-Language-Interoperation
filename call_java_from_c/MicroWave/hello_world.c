3ad217ed47ef5979162291810ef7ede73d560be9
niffery <niffery@example.com>
2018-05-19 08:43:56 -0700

    123

---
+++ call_java_from_c/MicroWave/hello_world.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <jni.h>
+
+JNIEnv* create_vm(JavaVM **jvm)
+{
+    JNIEnv* env;
+    JavaVMInitArgs args;
+    JavaVMOption options;
+    args.version = JNI_VERSION_1_8;
+    args.nOptions = 1;
+    options.optionString = "-Djava.class.path=./";
+    args.options = &options;
+    args.ignoreUnrecognized = 0;
+    jint rv;
+    rv = JNI_CreateJavaVM(jvm, (void**)&env, &args);
+    if (rv < 0 || !env)
+        printf("Unable to Launch JVM %d\n",rv);
+    else
+        printf("Launched JVM! :)\n");
+    return env;
+}
+
+void calling_method_test(JNIEnv* env)
+{
+    jobject Microwave_class;
+    jmethodID construct_method;
+    Microwave_class = (*env)->FindClass(env, "Microwave");
+    construct_method = (*env)->GetStaticMethodID(env, ,Microwave_class, "Microwave", "()V");   
+}
+int main(int argc, char **argv)
+{
+    JavaVM *jvm;
+    JNIEnv *env;
+    env = create_vm(&jvm);
+    if(env == NULL)
+        return 1;
+    invoke_class(env);
+    return 0;
+}
